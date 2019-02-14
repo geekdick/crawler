2d670ea15957dd65c1042a2adbb13e4fe5feee1e
刘学军 <liuxuejun@huox.tv>
2019-02-14 15:37:44 +0800

    rsa加密

--- encryption_algorithm/rsa_demo.py
+++ encryption_algorithm/rsa_demo.py
@@ -24,10 +24,40 @@ class RsaDemo(object):
         decrypt_text = rsa.decrypt(content, self.private_key)
         return decrypt_text.decode('utf-8')
 
+    def sign(self, message, private_key=None, hash_method='SHA-1'):
+        if not private_key:
+            private_key = self.private_key
+        return rsa.sign(message.encode(), private_key, hash_method)
+
+    def verify(self, message, encrypt_message, public_key=None):
+        if not public_key:
+            public_key = self.public_key
+        return rsa.verify(message, encrypt_message, public_key)
+
+    @staticmethod
+    def save_pem(pem_path, key_content):
+        with open(pem_path, 'bw') as f:
+            f.write(key_content.save_pkcs1())
+
+    @staticmethod
+    def read_pem(pem_path, is_public=True):
+        with open(pem_path, 'r') as f:
+            if is_public:
+                return rsa.PublicKey.load_pkcs1(f.read().encode())
+            else:
+                return rsa.PrivateKey.load_pkcs1(f.read().encode())
+
 
 if __name__ == '__main__':
     rsa_demo = RsaDemo()
+    rsa_demo.save_pem('public.pem', rsa_demo.public_key)
+    rsa_demo.save_pem('private.pem', rsa_demo.private_key)
+    print('public_pem', rsa_demo.read_pem('public.pem'))
+    print('private_pem', rsa_demo.read_pem('private.pem', is_public=False))
     text = 'abcd4321sdfsd'
     encrypt_result = rsa_demo.encrypt_oracle(text)
     print(encrypt_result)
     print(rsa_demo.decrypt_oracle(encrypt_result))
+    encrypt_text = rsa_demo.sign('nihaoya', private_key=rsa_demo.read_pem('private.pem', is_public=False))
+    print(encrypt_text)
+    print(rsa_demo.verify('nihaoya'.encode(), encrypt_text, public_key=rsa_demo.read_pem('public.pem')))
